5e57276662b8e29b61814fad26b27d6b385390c7
june6723 <june6723@naver.com>
2022-10-19 18:52:11 +0900

    storybook deploy

---
+++ storybook-static/assets/Button.stories.969a2578.js
@@ -0,0 +1,17 @@
+import{s as t}from"./styled-components.browser.esm.b659dad3.js";import"./index.269a798b.js";import"./iframe.c2fc4585.js";const e=t.button`
+  border: none;
+  border-radius: 0.5rem;
+  background-color: #186faf;
+  color: hsl(0deg, 0%, 98%);
+  padding: 0.75rem;
+  cursor: pointer;
+  &:hover {
+    background-color: #0a558c;
+  }
+  &:focus {
+    outline: none;
+    box-shadow: 0 0 0 2px #62b0e8;
+    background-color: #0a558c;
+  }
+`;try{e.displayName="Button",e.__docgenInfo={description:"",displayName:"Button",props:{ref:{defaultValue:null,description:"",name:"ref",required:!1,type:{name:"((instance: HTMLButtonElement | null) => void) | RefObject<HTMLButtonElement> | null"}},theme:{defaultValue:null,description:"",name:"theme",required:!1,type:{name:"any"}},as:{defaultValue:null,description:"",name:"as",required:!1,type:{name:"undefined"}},forwardedAs:{defaultValue:null,description:"",name:"forwardedAs",required:!1,type:{name:"undefined"}}}},typeof STORYBOOK_REACT_CLASSES<"u"&&(STORYBOOK_REACT_CLASSES["src/lib/Button.tsx#Button"]={docgenInfo:e.__docgenInfo,name:"Button",path:"src/lib/Button.tsx#Button"})}catch{}const d={title:"June Style/Button",component:e},l={args:{disabled:!1,children:"Hello"}},u=["Primary"];export{l as Primary,u as __namedExportsOrder,d as default};
+//# sourceMappingURL=Button.stories.969a2578.js.map
---
+++ storybook-static/assets/Input.stories.05d39e52.js
@@ -0,0 +1,16 @@
+import{s as n}from"./styled-components.browser.esm.b659dad3.js";import"./index.269a798b.js";import"./iframe.c2fc4585.js";const e=n.input`
+  border: 1px solid #222222;
+  border-radius: 0.2rem;
+  background-color: white;
+  color: #0c0c0c;
+  padding: 0.25rem 0.5rem;
+  cursor: pointer;
+  &:hover {
+    border-color: #2093e4;
+  }
+  &:focus {
+    outline: none;
+    border-color: #2093e4;
+  }
+`;try{e.displayName="Input",e.__docgenInfo={description:"",displayName:"Input",props:{ref:{defaultValue:null,description:"",name:"ref",required:!1,type:{name:"((instance: HTMLInputElement | null) => void) | RefObject<HTMLInputElement> | null"}},theme:{defaultValue:null,description:"",name:"theme",required:!1,type:{name:"any"}},as:{defaultValue:null,description:"",name:"as",required:!1,type:{name:"undefined"}},forwardedAs:{defaultValue:null,description:"",name:"forwardedAs",required:!1,type:{name:"undefined"}}}},typeof STORYBOOK_REACT_CLASSES<"u"&&(STORYBOOK_REACT_CLASSES["src/lib/Input.tsx#Input"]={docgenInfo:e.__docgenInfo,name:"Input",path:"src/lib/Input.tsx#Input"})}catch{}const d={title:"June Style/Input",component:e},l={args:{disabled:!1}},s=["Base"];export{l as Base,s as __namedExportsOrder,d as default};
+//# sourceMappingURL=Input.stories.05d39e52.js.map
